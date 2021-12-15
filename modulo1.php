1cdb1a601059749c5bea9ef9979ff76208428558
Lucas Montes <lucas.montes.lm@gmail.com>
2021-12-15 16:23:44 -0300

    Mesclando Strings

--- modulo1.php
+++ modulo1.php
@@ -3,4 +3,11 @@ $nome = "Lucas Montes"; //Variavel do tipo string
 $idade = 15; //variavel do tipo inteiro
 $dormindo = false; //variavel do tipo booleana
 $vazia = null;
-
+$array = [null, 10, "true", function(){}];
+var_dump($array); //Printa na tela o tipo de dado de cada elemento da lista
+echo "<br/> <br/>";
+$nome_1 = "Lucas";
+$sobrenome = "Montes";
+$nomecompleto = $nome_1;
+$nomecompleto .= $sobrenome;
+echo $nomecompleto;
