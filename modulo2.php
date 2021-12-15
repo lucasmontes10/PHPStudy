d194b05c4cf3974f2b9a7c86729888d7fe38e622
Lucas Montes <lucas.montes.lm@gmail.com>
2021-12-15 17:23:30 -0300

    Operador ternario

--- modulo2.php
+++ modulo2.php
@@ -5,3 +5,9 @@ if ($idade > 18){
 }else{
     echo "Menor de Idade";
 }
+
+$menor_idade = ($idade > 18)? true: false;
+
+echo "<br/> <br/>";
+echo $menor_idade;
+//Condicional ternario nada mais é que uma condicional escrita em apenas uma única linha
