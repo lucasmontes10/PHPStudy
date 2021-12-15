8e8ede3c66ccb9d9c22d2657237384df08c6d3e9
Lucas Montes <lucas.montes.lm@gmail.com>
2021-12-15 17:39:44 -0300

    condicional null caos

--- modulo2.php
+++ modulo2.php
@@ -11,3 +11,13 @@ $menor_idade = ($idade > 18)? true: false;
 echo "<br/> <br/>";
 echo $menor_idade;
 //Condicional ternario nada mais é que uma condicional escrita em apenas uma única linha
+
+
+//Condicional Null Cao
+//Muito parecido com o condicional ternário
+
+$nome_condicional = "Lucas";
+$nome_completo =  $nome_condicional;
+$nome_completo .= isset($sobrenome_condicional)? $sobrenome_condicional: ' ';//Ou seja se existir sobrenome ele escreve o próprio sobronome
+//caso contrario entrará com uma string vazia, isso é utilizado para evitar que de erro no codigo
+
