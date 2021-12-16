06ef71ed9709a71394a0776033157b7cac1c74fe
Lucas Montes <lucas.montes.lm@gmail.com>
2021-12-16 11:47:52 -0300

    Função anônima

--- modulo3.php
+++ modulo3.php
@@ -17,3 +17,15 @@ function Multiplicacao(int $a, int $b, int $c = 1){ // Esse valor de c é caso n
 }
 
 Multiplicacao(5, 2, 5);
+
+//Função anônima
+
+$funcao_Dizima = function(int $valor){
+    return $valor * 0.1;
+};
+
+$dizima = $funcao_Dizima;
+echo "<br/><br/>";
+echo $dizima(10);
+echo "<br/><br/>";
+echo $funcao_Dizima(50);
