abf32a6c419dc9045ba1edfdb9e4c8bd931dcad3
Lucas Montes <lucas.montes.lm@gmail.com>
2021-12-16 11:37:44 -0300

    Definindo valor padrão para uma função

--- modulo3.php
+++ modulo3.php
@@ -8,3 +8,12 @@ $x = 15;
 $y = 10;
 $soma = Soma($x, $y);
 echo $soma;
+
+echo "<br/><br/>";
+//Podemos tipar o que vai ser passado como parâmetro, por exemplo
+
+function Multiplicacao(int $a, int $b, int $c = 1){ // Esse valor de c é caso não seja passado o parâmetro
+    echo ($a * $b * $c);
+}
+
+Multiplicacao(5, 2, 5);
