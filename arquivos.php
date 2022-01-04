ce749aed23cbbe4f5d875b45ab64309dbaa75090
Lucas Montes <lucas.montes.lm@gmail.com>
2022-01-04 20:10:11 -0300

    Trabalhando com diferentes arquvos

---
+++ arquivos.php
@@ -0,0 +1,6 @@
+<?php
+//Nesse arquivo iremos tratar de arquivos em php
+//Motivação: Por exemplo um site com o mesmo cabeçalho e rodpé,seria interessante 
+//repetir o arquivo, pois já está criado
+require('modulo1.php');
+//Trouxe todo o contéudo do modulo 1
