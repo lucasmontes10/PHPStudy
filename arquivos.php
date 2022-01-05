0d743219ecd898f04607679b80b932a513592bf8
Lucas Montes <lucas.montes.lm@gmail.com>
2022-01-04 21:57:06 -0300

    trabalhando com require e include

--- arquivos.php
+++ arquivos.php
@@ -8,3 +8,8 @@ require_once('modulo1.php');
 include('modulonnexiste.php');
 //A unica diferença do include pro require é que aqui no include o código não para se o 
 //arquivo não existir
+
+
+//ATENÇÃO! FICAR BEM ATENTO COM AS PASTAS DIVERSAS POIS O QUE VALE É O ENDEREÇO ORIGINAL
+//E NÃO ONDE O ARQUIVO ESTÁ NO MOMENTO, BASTA PRESTAR ATENÇÃO NA PASTA ONDE É A ORIGEM NO MOMENTO
+//sE NECESSÁRIO REVISAR O CONTÉUDO DA ÚLTIMA LIÇÃO DO MODULO 3
